7ef4e7de496a864e4255362981109180efc32d89
tnagels <thomas@Nieuwe-Grijze-6.local>
2020-01-22 20:00:05 +0100

    Initial

---
+++ apps/novy/novy.py
@@ -0,0 +1,16 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+class NovyHoodControl(hass.Hass):
+
+    def initialize(self):
+        # Read settings
+        self.hood_name = self.args['hood']['name']
+        self.hood_speeds = self.args['hood']['speeds']
+        self.remote_id = self.args['remote']['id']
+        self.command_up = self.args['remote']['up']
+        self.command_down = self.args['remote']['down']
+        self.command_down = self.args['remote']['toggle']
+        # Create fan entity
+        if not self.entity_exists(self.name):
+            self.set_state(self.name, state = "off", attributes = {'speeds' = ['low','mid','high','boost']})
+        
